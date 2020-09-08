c99c5be38811de5bae7d51c1d632eb195745be73
IAmLuwenyang <2052987369@qq.com>
2020-09-09 00:24:27 +0800

    [优化] cart购物车配置文件、Cart实体类使用Lombox

--- pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Cart.java
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Cart.java
@@ -1,6 +1,10 @@
 package com.pinyougou.pojogroup;
 
 import com.pinyougou.pojo.TbOrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,35 +14,16 @@ import java.util.List;
  * @company HUST&华中科技大学
  * @create 2020-06-27 下午 03:27
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Cart implements Serializable {
 
+    private static final long serialVersionUID = -3200414670645810610L;
     private String sellerId;//商家ID
 
     private String sellerName;//商家名称
 
     private List<TbOrderItem> orderItemList;//购物车明细集合
-
-    public String getSellerId() {
-        return sellerId;
-    }
-
-    public void setSellerId(String sellerId) {
-        this.sellerId = sellerId;
-    }
-
-    public String getSellerName() {
-        return sellerName;
-    }
-
-    public void setSellerName(String sellerName) {
-        this.sellerName = sellerName;
-    }
-
-    public List<TbOrderItem> getOrderItemList() {
-        return orderItemList;
-    }
-
-    public void setOrderItemList(List<TbOrderItem> orderItemList) {
-        this.orderItemList = orderItemList;
-    }
 }
